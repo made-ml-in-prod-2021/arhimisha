b4c8bc32eb69dd4647272df16cbff5dde77d2bdc
Arhimisha <AndCloudOnTheAir>
2021-05-09 19:08:32 +0300

    add custom data transformer

--- ml_project/src/features_transformer/build_features_transformer.py
+++ ml_project/src/features_transformer/build_features_transformer.py
@@ -1,11 +1,26 @@
 import numpy as np
+import pandas as pd
+from typing import Union
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
+from sklearn.base import TransformerMixin
 
 from ..entities.feature_params import FeatureParams
+
+Array_2D = Union[pd.DataFrame, np.ndarray]
+
+class MyStandardScaler(TransformerMixin):
+    def transform(self, X: Array_2D, *_) ->Array_2D:
+        return (X - self.means) / self.std
+
+    def fit(self, X: Array_2D, *_) ->TransformerMixin:
+        self.means = np.mean(np.array(X), axis=0, keepdims=True)
+        self.std = np.std(np.array(X), axis=0, keepdims=True)
+        self.std[self.std == 0.] = 1
+        return self
 
 
 def build_categorical_pipeline() -> Pipeline:
@@ -22,7 +37,7 @@ def build_numerical_pipeline() -> Pipeline:
     num_pipeline = Pipeline(
         [
             ("impute", SimpleImputer(missing_values=np.nan, strategy="mean")),
-            ('scaler', StandardScaler())
+            ('scaler', MyStandardScaler())
         ]
     )
     return num_pipeline
