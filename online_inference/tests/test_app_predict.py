34b3cddbbf95c77606a266ccae0b8181006201e7
Arhimisha <miklpostbox@gmail.com>
2021-05-19 02:01:03 +0300

    fix test

--- online_inference/tests/test_app_predict.py
+++ online_inference/tests/test_app_predict.py
@@ -38,7 +38,7 @@ def test_predict_error(dataset_for_predict_path: str):
         data_with_wrong_features = XInput(data=data.values.tolist(), features=['wrong_features'])
         response = client.get("/predict/", json=data_with_wrong_features.dict())
         assert response.status_code == 400
-        data_with_wrong_data = XInput(data=[[1]], features=['wrong_features'])
+        data_with_wrong_data = XInput(data=[[1]], features=data.columns.tolist())
         response = client.get("/predict/", json=data_with_wrong_data.dict())
         assert response.status_code == 400
 
