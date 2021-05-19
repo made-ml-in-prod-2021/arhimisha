dbbb2d85e27f595adac515666a88da2d4fb88966
Arhimisha <miklpostbox@gmail.com>
2021-05-19 03:23:37 +0300

    Add dockerfile and fix make_request.py

--- online_inference/README.md
+++ online_inference/README.md
@@ -5,3 +5,10 @@
 `
 python make_request.py http://localhost/ data/test_data_for_predict.csv result.json
 `
+
+Команды Docker
+`
+docker build -t arhimisha/ml_in_prod_task_2:v1 .
+docker run -p 80:80 arhimisha/ml_in_prod_task_2:v1
+docker push arhimisha/ml_in_prod_task_2:v1
+`
--- online_inference/make_request.py
+++ online_inference/make_request.py
@@ -23,14 +23,13 @@ def make_predict(server_address: str,
     data_for_predict = XInput(data=data.values.tolist(), features=data.columns.tolist())
     logger.info(f"send request")
     response = requests.get(
-        urllib.parse.urljoin(server_address, "predictdd"),
+        urllib.parse.urljoin(server_address, "predict"),
         json=data_for_predict.dict(),
     )
     logger.info(f"parse result")
     result=[]
     if (response.status_code == 200):
         for elem in response.json():
-            print(elem)
             predict = YResponse(**elem)
             result.append(predict.predict)
 
@@ -41,19 +40,6 @@ def make_predict(server_address: str,
         logger.info(f"save result")
         with open(path_for_save_result, "w") as result_file:
             json.dump(response.json(), result_file)
-
-    # for i in range(len(response)):
-    #     request_data = [
-    #         x.item() if isinstance(x, np.generic) else x for x in data.iloc[i].tolist()
-    #     ]
-    #     print(request_data)
-    #     response = requests.get(
-    #         "http://3.127.229.49/predict/",
-    #         json={"data": [request_data], "features": request_features},
-    #     )
-    #     print(response.status_code)
-    #     print(response.json())
-
 
 @click.command(name="make_predict")
 @click.argument("server_address")
