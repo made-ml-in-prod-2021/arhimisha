41a1cbae82d29ea48ef5d825d2c4e1356d75bc73
Arhimisha <AndCloudOnTheAir>
2021-05-24 11:48:00 +0300

    fix logger in app_predict.py

--- online_inference/app_predict.py
+++ online_inference/app_predict.py
@@ -14,7 +14,6 @@ from sklearn.pipeline import Pipeline
 from src.entities.fearures_info import FeaturesInfo, read_features_info
 
 logger = logging.getLogger(__name__)
-handler = logging.StreamHandler(sys.stdout)
 log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.INFO, format=log_fmt)
 
